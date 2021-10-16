febb3b7957343b29b4725b00f0c171c13e9450d6
Siddhartha Banerjee <sidban@uwalumni.com>
2021-10-16 10:18:37 -0700

    One dimensional Gas Dynamics
    Deployed 1d CFD gas dynamics based on predictor and corrector method.

---
+++ _examples/1d-flow.py
@@ -0,0 +1,82 @@
+## Import packages and modules
+import numpy as np
+import matplotlib.pyplot as plt
+from cfdlite.onedim import PipeModel as Model
+from cfdlite.onedim import FlowSolver as Solver
+from cfdlite.onedim import BoundaryType as Type
+from cfdlite.onedim import BoundaryCategory as Category
+## Setup model
+x_mesh = np.linspace(0, 1, 1000)
+area = np.full(x_mesh.shape, np.pi * 0.1 ** 2 / 4)
+dA = (np.cos(2 * np.pi * x_mesh) - 1) / 2 * area[0] / 2
+area += dA
+gamma = 1.4
+mdl = Model(
+    x=x_mesh,
+    area=area,
+    perimeter=np.full_like(x_mesh, 1),
+    mdot_w=np.full_like(x_mesh, 0),
+    tau_w=np.full_like(x_mesh, 0),
+    q_w=np.full_like(x_mesh, 0),
+)
+## Specify initial condition
+uu_initial = np.array(
+    [
+        np.full_like(x_mesh, 1.0) * area,
+        np.full_like(x_mesh, 0.0) * area,
+        np.full_like(x_mesh, 250000) * area,
+    ]
+)
+flux = mdl.flux(uu=uu_initial,
+                gamma=np.full_like(x_mesh, gamma))
+thermo = mdl.thermo(uu=uu_initial,
+                    gamma=np.full_like(x_mesh, gamma))
+source = mdl.source(uu=uu_initial,
+                    gamma=np.full_like(x_mesh, gamma))
+## Specify boundary conditions
+intake = {
+    Category.MF: {Type.NE: float(0)},
+    Category.TP: {Type.DI: float(120000)},
+    Category.TH: {Type.NE: float(0)},
+}
+outlet = {
+    Category.MF: {Type.NE: float(0)},
+    Category.TP: {Type.DI: float(100000)},
+    Category.TH: {Type.NE: float(0)},
+}
+soln = Solver(model=mdl, initial_solution=uu_initial,
+              intake_boundary=intake, outlet_boundary=outlet)
+soln.gamma = np.full_like(x_mesh, gamma)
+##
+soln.update()
+mdl.thermo(uu=soln.uu, gamma=soln.gamma)
+## Time marching
+for i in range(int(2500)).__iter__():
+    soln.update()
+## Plot the final state
+fig = plt.figure('Fig: Solution at final time')
+axs = fig.subplots(nrows=4, ncols=1, sharex=True)
+axs[0].plot(
+    x_mesh, mdl.thermo(uu=soln.uu, gamma=soln.gamma)['velocity'],
+)
+axs[0].set_ylabel('[m/s]')
+axs[0].set_title('Velocity')
+axs[1].plot(
+    x_mesh, mdl.thermo(uu=soln.uu, gamma=soln.gamma)['pressure'] / 1e5,
+)
+axs[1].set_ylabel('[bar]')
+axs[1].set_title('Pressure')
+axs[2].plot(
+    x_mesh, mdl.thermo(uu=soln.uu, gamma=soln.gamma)['density'],
+)
+axs[2].set_ylabel('[kg/s]')
+axs[2].set_title('Density')
+axs[3].plot(
+    x_mesh,
+    mdl.thermo(uu=soln.uu, gamma=soln.gamma)['internal_energy'] / 714.28,
+)
+axs[3].set_ylabel('[K]')
+axs[3].set_title('Temperature')
+axs[3].set_xlabel('x [m]')
+_ = [ax.grid(True) for ax in axs.flat]
+##
---
+++ cfdlite/onedim.py
@@ -0,0 +1,352 @@
+"""
+Solvers for one-dimensional computational fluid dynamics.
+
+@author: siddhartha.banerjee
+"""
+
+import numpy as np
+from enum import Enum, unique
+
+
+class PipeModel:
+    """Functions and properties of the CFD domain."""
+
+    def __init__(
+        self,
+        x: np.array = np.nan,
+        area: np.array = np.nan,
+        mdot_w: np.array = np.nan,
+        tau_w: np.array = np.nan,
+        q_w: np.array = np.nan,
+        perimeter: np.array = np.nan,
+    ) -> None:
+        """Instantiate the class."""
+        self.dA_dx = np.gradient(
+            area, x,
+            edge_order=1,
+        )
+        self.x = x
+        self.area = area
+        self.mdot_w = mdot_w
+        self.tau_w = tau_w
+        self.q_w = q_w
+        self.perimeter = perimeter
+        self.__ff0 = np.vectorize(
+            lambda uu0, uu1, uu2, gamma: uu1
+        )
+        self.__ff1 = np.vectorize(
+            lambda uu0, uu1, uu2, gamma: (uu1 ** 2) / uu0 + (
+                uu0 * (uu2 / uu0 - 0.5 * (uu1 / uu0) ** 2) * (gamma - 1)
+            )
+        )
+        self.__ff2 = np.vectorize(
+            lambda uu0, uu1, uu2, gamma: uu1 * (
+                gamma * uu2 / uu0 - (gamma - 1) / 2 * (uu1 / uu0) ** 2
+            )
+        )
+        self.__g0 = np.vectorize(
+            lambda uu0, uu1, uu2, mdot_w_x, l: mdot_w_x * l
+        )
+        self.__g1 = np.vectorize(
+            lambda uu0, uu1, uu2, gamma, area_x, dela_delx, tau_w_x, l: (
+                uu2 / uu0 - 0.5 * (uu1 / uu0) ** 2
+            ) * uu0 / area_x * (gamma - 1) * dela_delx - tau_w_x * l
+        )
+        self.__g2 = np.vectorize(
+            lambda uu0, uu1, uu2, q_x, l: q_x * l
+        )
+        self.__velocity = np.vectorize(
+            lambda uu0, uu1, uu2: uu1 / uu0
+        )
+        self.__density = np.vectorize(
+            lambda uu0, uu1, uu2, area_x: uu0 / area_x
+        )
+        self.__internal_energy = np.vectorize(
+            lambda uu0, uu1, uu2: (uu2 / uu0) - (
+                0.5 * (uu1 / uu0) ** 2
+            )
+        )
+        self.__pressure = np.vectorize(
+            lambda uu0, uu1, uu2, area_x, gamma: (
+                self.__internal_energy(
+                    uu0, uu1, uu2)
+            ) * (
+                self.__density(uu0, uu1, uu2, area_x)
+            ) * (gamma - 1)
+        )
+
+    def flux(
+        self,
+        uu: np.array = np.nan,
+        gamma: np.array = np.nan,
+    ) -> np.array:
+        """Get flux vector."""
+        return np.array(
+            [
+                self.__ff0(uu[0], uu[1], uu[2], gamma),
+                self.__ff1(uu[0], uu[1], uu[2], gamma),
+                self.__ff2(uu[0], uu[1], uu[2], gamma),
+            ],
+        )
+
+    def source(
+        self,
+        uu: np.array = np.nan,
+        gamma: np.array = np.nan,
+    ) -> np.array:
+        """Get source vector."""
+        return np.array(
+            [
+                self.__g0(uu[0], uu[1], uu[2], self.mdot_w, self.perimeter),
+                self.__g1(uu[0], uu[1], uu[2], gamma, self.area, self.dA_dx,
+                          self.tau_w, self.perimeter),
+                self.__g2(uu[0], uu[1], uu[2], self.q_w, self.perimeter),
+            ],
+        )
+
+    def thermo(
+        self,
+        uu: np.array = np.nan,
+        gamma: np.array = np.nan,
+    ) -> dict:
+        """Get the thermodynamic properties."""
+        return dict(
+            {
+                'velocity': self.__velocity(uu[0], uu[1], uu[2]),
+                'density': self.__density(uu[0], uu[1], uu[2], self.area),
+                'pressure': self.__pressure(uu[0], uu[1], uu[2], self.area,
+                                            gamma),
+                'internal_energy': self.__internal_energy(uu[0], uu[1], uu[2]),
+            }
+        )
+
+
+class FlowSolver:
+    """Finite Volume scheme for non-linear inhomogeneous transport."""
+
+    def __init__(
+        self,
+        model: PipeModel = None,
+        initial_solution: np.array = np.nan,
+        intake_boundary: dict = None,
+        outlet_boundary: dict = None,
+    ):
+        """Instantiate the class."""
+        self.model = model
+        self.uu = initial_solution
+        self.gamma = np.nan
+        if intake_boundary is None:
+            self.intake_boundary = {
+                BoundaryCategory.MF: {BoundaryType.DI: float(0)},
+                BoundaryCategory.TP: {BoundaryType.NE: float(0)},
+                BoundaryCategory.TH: {BoundaryType.DI: float(0)},
+            }
+        else:
+            self.intake_boundary = intake_boundary
+        if outlet_boundary is None:
+            self.outlet_boundary = {
+                BoundaryCategory.MF: {BoundaryType.NE: float(0)},
+                BoundaryCategory.TP: {BoundaryType.NE: float(0)},
+                BoundaryCategory.TH: {BoundaryType.NE: float(0)},
+            }
+        else:
+            self.outlet_boundary = outlet_boundary
+
+    def __get_boundary_flux_for_predictor(
+        self,
+        uu: np.array = np.nan,
+        gamma: np.array = np.nan,
+    ) -> np.array:
+        """Get boundary flux terms for predictor."""
+        x = self.model.x
+        flux = self.model.flux(uu=uu, gamma=gamma)
+        outlet = self.outlet_boundary
+        mfr_outlet = np.nan
+        total_pressure_outlet = np.nan
+        total_enthalpy_outlet = np.nan
+        scale = [
+            1,
+            self.model.area[-1],
+            uu[1][-1],
+        ]
+        offset = [
+            0,
+            0.5 * (uu[1][-1] ** 2.0) / uu[0][-1],
+            0,
+        ]
+        if outlet[BoundaryCategory.MF].keys().__contains__(BoundaryType.DI):
+            mfr_outlet = np.fromiter(
+                outlet[BoundaryCategory.MF].values(),
+                dtype=float,
+            )[0] * scale[0] + offset[0]
+        elif outlet[BoundaryCategory.MF].keys().__contains__(BoundaryType.NE):
+            mfr_outlet = flux[0][-1] + np.fromiter(
+                outlet[BoundaryCategory.MF].values(),
+                dtype=float,
+            )[0] * (x[-1] - x[-2]) * scale[0] + offset[0]
+        if outlet[BoundaryCategory.TP].keys().__contains__(BoundaryType.DI):
+            total_pressure_outlet = np.fromiter(
+                outlet[BoundaryCategory.TP].values(),
+                dtype=float,
+            )[0] * scale[1] + offset[1]
+        elif outlet[BoundaryCategory.TP].keys().__contains__(BoundaryType.NE):
+            total_pressure_outlet = flux[1][-1] + np.fromiter(
+                outlet[BoundaryCategory.TP].values(),
+                dtype=float,
+            )[0] * (x[-1] - x[-2]) * scale[1] + offset[1]
+        if outlet[BoundaryCategory.TH].keys().__contains__(BoundaryType.DI):
+            total_enthalpy_outlet = np.fromiter(
+                outlet[BoundaryCategory.TH].values(),
+                dtype=float,
+            )[0] * scale[2] + offset[2]
+        elif outlet[BoundaryCategory.TH].keys().__contains__(BoundaryType.NE):
+            total_enthalpy_outlet = flux[2][-1] + np.fromiter(
+                outlet[BoundaryCategory.TH].values(),
+                dtype=float,
+            )[0] * (x[-1] - x[-2]) * scale[2] + offset[2]
+        return np.array(
+            [
+                mfr_outlet,
+                total_pressure_outlet,
+                total_enthalpy_outlet,
+            ]
+        )
+
+    def __get_boundary_flux_for_corrector(
+        self,
+        uu: np.array = np.nan,
+        gamma: np.array = np.nan,
+    ) -> np.array:
+        """Get boundary flux terms for corrector."""
+        x = self.model.x
+        flux = self.model.flux(uu=uu, gamma=gamma)
+        intake = self.intake_boundary
+        mfr_intake = np.nan
+        total_pressure_intake = np.nan
+        total_enthalpy_intake = np.nan
+        scale = [
+            1,
+            self.model.area[0],
+            uu[1][0],
+        ]
+        offset = [
+            0,
+            0.5 * (uu[1][0] ** 2.0) / uu[0][0],
+            0,
+        ]
+        if intake[BoundaryCategory.MF].keys().__contains__(BoundaryType.DI):
+            mfr_intake = np.fromiter(
+                intake[BoundaryCategory.MF].values(),
+                dtype=float,
+            )[0] * scale[0] + offset[0]
+        elif intake[BoundaryCategory.MF].keys().__contains__(BoundaryType.NE):
+            mfr_intake = flux[0][0] - np.fromiter(
+                intake[BoundaryCategory.MF].values(),
+                dtype=float,
+            )[0] * (x[1] - x[0]) * scale[0] - offset[0]
+        if intake[BoundaryCategory.TP].keys().__contains__(BoundaryType.DI):
+            total_pressure_intake = np.fromiter(
+                intake[BoundaryCategory.TP].values(),
+                dtype=float,
+            )[0] * scale[1] + offset[1]
+        elif intake[BoundaryCategory.TP].keys().__contains__(BoundaryType.NE):
+            total_pressure_intake = flux[1][0] - np.fromiter(
+                intake[BoundaryCategory.TP].values(),
+                dtype=float,
+            )[0] * (x[1] - x[0]) * scale[1] - offset[1]
+        if intake[BoundaryCategory.TH].keys().__contains__(BoundaryType.DI):
+            total_enthalpy_intake = np.fromiter(
+                intake[BoundaryCategory.TH].values(),
+                dtype=float,
+            )[0] * scale[2] + offset[2]
+        elif intake[BoundaryCategory.TH].keys().__contains__(BoundaryType.NE):
+            total_enthalpy_intake = flux[2][0] - np.fromiter(
+                intake[BoundaryCategory.TH].values(),
+                dtype=float,
+            )[0] * (x[1] - x[0]) * scale[2] - offset[2]
+        return np.array(
+            [
+                mfr_intake,
+                total_pressure_intake,
+                total_enthalpy_intake,
+            ]
+        )
+
+    @staticmethod
+    def __predictor(
+        x: np.array = np.nan,
+        flux: np.array = np.nan,
+        source: np.array = np.nan,
+        outlet_flux: np.array = np.nan,
+    ) -> np.array:
+        """Predictor step.
+        :type outlet_flux: numpy.ndarray
+        """
+        return - np.diff(
+            np.append(flux.T, np.array([outlet_flux]), axis=0).T,
+            n=int(1),
+            axis=1,
+        ) / np.diff(
+            x,
+            n=int(1),
+            append=2 * x[-1] - x[-2],
+        ) + source
+
+    @staticmethod
+    def __corrector(
+        x: np.array = np.nan,
+        flux: np.array = np.nan,
+        source: np.array = np.nan,
+        intake_flux: np.array = np.nan,
+    ) -> np.array:
+        """Corrector step.
+        :type intake_flux: numpy.ndarray
+        """
+        return - np.diff(
+            np.append(np.array([intake_flux]), flux.T, axis=0).T,
+            n=int(1),
+            axis=1,
+        ) / np.diff(
+            np.insert(arr=x, obj=0, values=2 * x[0] - x[1]),
+            n=int(1),
+        ) + source
+
+    def update(
+        self,
+        dt: np.float = 1e-6,
+    ) -> None:
+        """Update the solution for next time step."""
+        # //todo: Update Gamma and Pass on Inlet and Outlet boundaries
+        uu_predictor = self.uu + dt * (
+            self.__predictor(
+                x=self.model.x,
+                flux=self.model.flux(uu=self.uu, gamma=self.gamma),
+                source=self.model.source(uu=self.uu, gamma=self.gamma),
+                outlet_flux=self.__get_boundary_flux_for_predictor(
+                    uu=self.uu, gamma=self.gamma),
+            )
+        )
+        uu_corrector = self.uu + dt * (
+            self.__corrector(
+                x=self.model.x,
+                flux=self.model.flux(uu=uu_predictor, gamma=self.gamma),
+                source=self.model.source(uu=uu_predictor, gamma=self.gamma),
+                intake_flux=self.__get_boundary_flux_for_corrector(
+                    uu=uu_predictor, gamma=self.gamma),
+            )
+        )
+        self.uu = 0.5 * (uu_predictor + uu_corrector)
+
+
+@unique
+class BoundaryType(Enum):
+    DI = 'Specified Value (Dirichlet)'
+    NE = 'Specified Normal Gradient (Neumann)'
+    DR = 'Derived Boundary'
+
+
+@unique
+class BoundaryCategory(Enum):
+    MF = 'Mass flow rate'
+    TP = 'Total Pressure'
+    TH = 'Total Enthalpy'
